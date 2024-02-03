06d81d4a5a26f97886c5b01c8933fa22ef676045
Xane Snyder <142278744+Xetcrosis@users.noreply.github.com>
2024-02-03 17:40:42 -0500

    work on api integration

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-slint::slint! {
-    export component App inherits Window {
-        GridLayout {
+use dotenv::dotenv;
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
+use std::env;
 
-        }
+async fn grab_verses() -> HeaderMap {
+    dotenv().ok();
 
-    }
+    let api_key = env::var("BIBLE_API_KEY").expect("| Code error | failed to find the api key");
+
+    let api_grab: String = format!("api-key: {}", api_key);
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "api-key",
+        HeaderValue::from_str(&api_grab).expect("Invalid header value"),
+    );
+    return headers;
 }
 
 fn main() {
-    let app: App = App::new().unwrap().run().unwrap();
-    let weak: Weak<App> = app.as_weak();
+    //    let app: App = App::new().unwrap().run().unwrap();
+    //    let weak: Weak<App> = app.as_weak();
 }
-
-// use reqwest::header::{HeaderMap, AUTHORIZATION, HeaderValue, HeaderName};
-// use serde::Deserialize;
-
-
-// #[tokio::main]
-// async fn smth() {
-//     let request_url = format!("https://api.scripture.api.bible/v1/bibles")
-//     let body = reqwest::get()
-// }
