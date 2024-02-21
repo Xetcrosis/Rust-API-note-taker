cce06aca0e9acc88815db5aa09310df497a5a20e
Xetta Snyder <xcrosis@gmail.com>
2024-02-21 14:13:01 -0500

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use reqwest::blocking::{Client,ClientBuilder};
+use reqwest::{blocking::{Client,ClientBuilder}, header};
 use std::env;
 
 slint::slint! {
@@ -11,17 +11,18 @@ slint::slint! {
 }
 // dotenv().ok();
 
-// let api_key = env::var("BIBLE_API_KEY").expect("| Code error | failed to find the api key");
 
 
 fn main() {
+    let api_key: String = env::var("BIBLE_API_KEY").expect("| Code error | failed to find the api key");
     let url: &str = "https://api.scripture.api.bible/v1/bibles";
     let http_client:Client = Client::new();
     let http_result = http_client.get(url).send();
+    headers.insert("API-key", header::HeaderValue::fromstr(api_key).unwrap());
 
     if http_result.is_ok() {
         println!("response ok {:#?}", http_result.ok().unwrap());
     } else {
-        print!("PROGRESS!! failure but progress.")
+        print!("It dont do")
     }
 }
